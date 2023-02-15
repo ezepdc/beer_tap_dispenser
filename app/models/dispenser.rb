4075cf4e661fe95447fcf9f6da0ec501b2442664
Ezequiel <epuyosdacosta@gmail.com>
2023-02-15 18:08:50 +0100

    Calculation test

--- app/models/dispenser.rb
+++ app/models/dispenser.rb
@@ -2,4 +2,8 @@ class Dispenser < ApplicationRecord
   belongs_to :user
   has_many :spendings
   validates :flow_volume, presence: true
+
+  def calc
+    (closed_at.to_i - opened_at.to_i) * flow_volume.to_i * price.to_i
+  end
 end
